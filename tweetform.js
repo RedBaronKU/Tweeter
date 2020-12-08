4d4eefbcc40c5f48a9863d2174f44f6444e10bf5
RedBaronKU <kr.utkarsh106@gmail.com>
2020-12-08 11:49:54 +0530

    first commit

---
+++ tweetform.js
@@ -0,0 +1,16 @@
+const form=document.getElementById("formtext")
+form.addEventListener('submit',function(e)
+{
+    e.preventDefault();
+     const text1=document.querySelector('#Username');
+     const text2=document.querySelector('#Tweet');
+     const bold=document.createElement('b');
+     const tweeting=document.createElement('li');
+     bold.append('@'+text1.value+':    ');
+     tweeting.append(bold);
+     tweeting.append(text2.value);
+     if(text1.value!=''&&text2.value!='')
+        form.append(tweeting);
+     text1.value='';
+     text2.value='';
+});
