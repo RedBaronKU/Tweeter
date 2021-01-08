5734b3e6cee995c5baa45686d7b45e07ec26a968
RedBaronKU <kr.utkarsh106@gmail.com>
2021-01-08 15:11:42 +0530

    testgrid

--- tweetform.js
+++ tweetform.js
@@ -10,8 +10,30 @@ form.addEventListener('submit',function(e)
      bold.append('@'+text1.value+':    ');
      tweeting.append(bold);
      tweeting.append(text2.value);
-     if(text1.value!=''&&text2.value!='')
+     if(text1.value!=''&& text2.value!='')
         tweets.append(tweeting);
      text1.value='';
      text2.value='';
 });
+function jokeAdd(joke)
+{
+    const ul=document.getElementById('tweet_display');
+    const li=document.createElement('li');
+    const bold=document.createElement('b');
+    bold.append('@randonJoke:    ');
+    li.append(bold);
+    li.append(`${joke}`);
+    ul.append(li);
+    console.log(joke)
+}
+async function dadJoke()
+{
+   	var config = {headers:{Accept: 'application/json'}};
+   	var res = await axios.get('https://icanhazdadjoke.com/',config);
+   	jokeAdd(res.data.joke)
+}
+const formjoke=document.getElementById("form");
+formjoke.addEventListener('submit',function(e){
+    e.preventDefault();
+    dadJoke();
+})
