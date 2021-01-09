14c3e4a6780c1d0651774c07f937466618a3871a
RedBaronKU <kr.utkarsh106@gmail.com>
2021-01-09 16:18:33 +0530

    NewsAPI_added(not working)

--- tweetform.js
+++ tweetform.js
@@ -6,9 +6,9 @@ form.addEventListener('submit',function(e)
      const text2=document.querySelector('#tweet');
      const bold=document.createElement('b');
      const tweeting=document.createElement('li');
-     tweeting.style.borderBottom ="2px solid red";
+     tweeting.style.borderBottom ="2px solid  rgb(105, 105, 248)";
      const tweets=document.querySelector('#tweet_display')
-     bold.append('@'+text1.value+':    ');
+     bold.append(`@${text1.value}:`);
      tweeting.append(bold);
      tweeting.append(text2.value);
      if(text1.value!=''&& text2.value!='')
@@ -16,6 +16,7 @@ form.addEventListener('submit',function(e)
      text1.value='';
      text2.value='';
 });
+var bres;
 function jokeAdd(joke)
 {
     const ul=document.getElementById('tweet_display');
@@ -32,11 +33,21 @@ async function dadJoke()
    	var res = await axios.get('https://icanhazdadjoke.com/',config);
    	jokeAdd(res.data.joke)
 }
+async function getNews()
+{
+    //const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const url= "http://newsapi.org/v2/top-headlines?country=in&apiKey=0195b2590f9c4187843064590c5ceb10";
+    bres = await axios.get(`${url}`);
+    console.log(bres)
+}
 const formjoke=document.getElementById("form");
 formjoke.addEventListener('submit',function(e){
     e.preventDefault();
     dadJoke();
+    //getNews();
 })
+
+//characterCount
 var el;                                                    
 function countCharacters(e) {                                    
   var textEntered, countRemaining, counter;          
@@ -47,7 +58,6 @@ function countCharacters(e) {
   if(counter<0)
   {  
      document.getElementById("tweetPoster").disabled = true;
-     counter=0;
   }
   if(counter>=0)
   {
@@ -56,3 +66,7 @@ function countCharacters(e) {
 }
 el = document.getElementById('tweet');                   
 el.addEventListener('keyup', countCharacters);
+
+
+
+
